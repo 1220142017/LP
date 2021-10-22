3a5e7546256c3170e1ba0979679664c7fd321e71
1220142017 <92655811+1220142017@users.noreply.github.com>
2021-10-22 15:06:25 -0300

    Create 1001.c

---
+++ 1001.c
@@ -0,0 +1,13 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+    int a,b,x;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    x = a + b;
+    printf("X = %d",x);
+    printf("\n");
+    return 0;
+}
