60e7a4a4c44903ff1f466c26b3bc8219913b9ef8
1220142017 <92655811+1220142017@users.noreply.github.com>
2021-11-01 04:25:25 -0300

    Create 2483.c

---
+++ 2483.c
@@ -0,0 +1,12 @@
+int main(){
+    int i, num;
+    scanf("%d",&num);
+    
+    printf("Feliz nat");
+    
+    for(i=0;i<num;i++){
+        printf("a");
+    }
+    printf("l!\n");
+    return 0;
+}
