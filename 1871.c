c10d27d835e2626e7cc1d8bb25e3ac1972efe61e
Natália Mercês de Oliveira <92655811+NataliaMercesdeOliveira@users.noreply.github.com>
2021-11-25 17:02:19 -0300

    Update 1871.c

--- 1871.c
+++ 1871.c
@@ -1,1 +1,35 @@
+#include <stdlib.h>
+#include <stdio.h>
 
+int main(){
+    char soma[18];
+    int i,aux,m,n;
+    
+    for(i=0;i<18;i++){
+        soma[i] == NULL;
+    }
+    
+    scanf("%d %d",&n,&m);
+    
+    while(n != 0 && m != 0){
+        aux = n+m;
+        sprintf(soma,"%d",aux);
+        
+    for(i=0;i<strlen(soma);i++){
+        if(soma[i] != '0') printf("%c",soma[i]);
+    }
+    printf("\n");
+    
+    
+    
+    for(i=0;i<18;i++){
+        soma[i] == NULL;
+    }
+    
+    scanf("%d %d",&n,&m);    
+        
+    }
+    
+    
+    return 0;
+}
