7d51d4d243ab4520770fc5eeeb6b4474d0fe6909
Natália Mercês de Oliveira <92655811+NataliaMercesdeOliveira@users.noreply.github.com>
2021-12-03 03:25:58 -0300

    Create 1014.c

---
+++ 1014.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    float x,y,dist;
+    
+    scanf("%f",&x);
+    scanf("%f",&y);
+    dist = x/y;
+    printf("%.3f km/l\n",dist);
+    
+    return 0;
+}
